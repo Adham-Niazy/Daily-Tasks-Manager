196ef25c66b6a4c641d76b6d8dc50bbee7272cad
Adham-Niazy <adhamniazy@gmail.com>
2023-02-26 03:33:55 +0200

    Handling (Add Task) UI + Functionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,41 @@
 <template>
-  <div>Hello World!! {{name}}</div>
+  <main>
+    <h1>Daily Tasks Board</h1>
+    <p>Create your list of daily tasks</p>
+
+    <div class="create-new">
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Add a new task"
+        @keypress.enter="addTask"
+      />
+
+      <button @click="addTask">Add</button>
+    </div>
+
+    <div class="tasks">
+      <!-- COMPONENT -->
+
+      {{ $store.state.tasks }}
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      newTask: ""
+    }
+  },
+  methods: {
+    addTask() {
+      if ( this.newTask && this.newTask.length >= 3) {
+        this.$store.commit('ADD_TASK', this.newTask)
+        this.newTask = '';
+      }
+    }
+  }
+};
 </script>
