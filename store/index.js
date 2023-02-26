c51aefa784e60d971469a917383c909c30e12caf
Adham-Niazy <adhamniazy@gmail.com>
2023-02-26 05:13:38 +0200

    Adding Local Storage Logic

--- store/index.js
+++ store/index.js
@@ -1,18 +1,32 @@
 export const state = () => ({
-  tasks: []
+  tasks: getFromDataSource(DATA_SOURCE_KEY) || [],
 });
 
 export const mutations = {
   ADD_TASK (state, task) {
     state.tasks = [ { id: new Date().toISOString()+task, content: task, done: false }, ...state.tasks ];
+    updateDataSource(DATA_SOURCE_KEY, state.tasks);
   },
 
   REMOVE_TASK ( state, task ) {
     let FilteredTasks = state.tasks.filter( (t) => t.id !== task.id );
     state.tasks = FilteredTasks;
+    updateDataSource(DATA_SOURCE_KEY, state.tasks);
   },
 
   TOGGLE_TASK (state, task) {
     task.done = !task.done;
+    updateDataSource(DATA_SOURCE_KEY, state.tasks);
   }
+}
+
+/// UTILS (LOCAL STORAGE) Methods
+const DATA_SOURCE_KEY = 'DAILY_TASKS';
+
+function updateDataSource(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+function getFromDataSource(key) {
+  return JSON.parse(localStorage.getItem(key));
 }
