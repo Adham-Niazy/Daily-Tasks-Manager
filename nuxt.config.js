6e15f827a6fa57174417519d6a23cf5abff55311
Adham-Niazy <adhamniazy@gmail.com>
2023-02-26 04:55:23 +0200

    Changing Application Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'daily-tasks-manager',
+    title: 'Daily Task Manager',
     htmlAttrs: {
       lang: 'en'
     },
